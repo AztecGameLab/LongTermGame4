f71012b77684752ec35d051d7554c0ca1ee499ba
MattWant <71905800+MattWant@users.noreply.github.com>
2020-10-04 15:11:07 -0700

    Started the Player Shooting element

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public float speed = 8f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Makes the Arrow move
+        transform.position += transform.forward * speed * Time.deltaTime;
+        
+    }
+}
