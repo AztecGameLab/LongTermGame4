bd2d4c73cf86955e4a24c7f2852a3ce716c6a1f3
mattkmaier <70078107+mattkmaier@users.noreply.github.com>
2020-10-05 14:01:30 -0700

    Added triggerOnExit function

--- Assets/Scripts/ArrowTriggeredButton.cs
+++ Assets/Scripts/ArrowTriggeredButton.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class ArrowTriggeredButton : MonoBehaviour{
 
    [SerializeField] GameObject triggeredObject;
-   bool isTriggered = false;
+
 
     private void OnTriggerEnter(Collider other){
-
         if (other.CompareTag("arrow")){
             //Modify the triggered object's state depending on the mechanics used
             //Example used here is changing the material on the indicator block
             triggeredObject.GetComponent<Renderer>().material.color = Color.green;
         }
-
     }
 
+    private void OnTriggerExit(Collider other){
+        if (other.CompareTag("arrow")){
+            //Modify the triggered object's state depending on the mechanics
+            triggeredObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
 }
+
+//May need to revisit script depending on how arrow recall functionally
