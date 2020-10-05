7a1a0fd0ac94582b7c97c4cc9a5d068836a214cd
MattWant <71905800+MattWant@users.noreply.github.com>
2020-10-05 00:06:41 -0700

    Fixed what was recommended
    Made everything work in one script using the rigidbody and addforce

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerShooting : MonoBehaviour
 {
     public GameObject arrowPrefab;
+    public float speed = 100f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
@@ -17,9 +18,11 @@ public class Player : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject arrowObject = Instantiate(arrowPrefab);
+            GameObject arrowObject = Instantiate(arrowPrefab) as GameObject;
             arrowObject.transform.position = transform.position + transform.forward;
             arrowObject.transform.forward = transform.forward;
+            arrowObject.GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+            
         }
     }
 }
