69b21c14bbf680e808091254d57db839a78328bc
mattkmaier <70078107+mattkmaier@users.noreply.github.com>
2020-10-05 13:32:15 -0700

    Set up on triger zone to accept collision with arrows

---
+++ Assets/Scripts/ArrowTriggeredButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowTriggeredButton : MonoBehaviour{
+
+   [SerializeField] GameObject triggeredObject;
+   bool isTriggered = false;
+
+    private void OnTriggerEnter(Collider other){
+
+        if (other.CompareTag("arrow")){
+            //Modify the triggered object's state depending on the mechanics used
+            //Example used here is changing the material on the indicator block
+            triggeredObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+
+    }
+
+}
