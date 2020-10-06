4719791294e3f12d8a077fe0e5e8db0ae467a6b5
maez-vid <meziiro@gmail.com>
2020-10-05 19:38:22 -0700

    Character movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public CharacterController charController;
+    public float moveSpeed = 10f;
+    public float gravity = -9.81f;
+
+    Vector3 velocity;
+    private bool isGrounded;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,16 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        charController.Move(move * moveSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        charController.Move(velocity * Time.deltaTime);
     }
 }
