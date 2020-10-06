5ee45bc9d1e3826c379dccacddf19f3d118ed61e
MatthewMorisky <matthew.morisky@gmail.com>
2020-10-05 18:57:01 -0600

    Update PlayerShooting.cs

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -4,25 +4,37 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
+    
     public GameObject arrowPrefab;
-    public float speed = 100f;
+    public float shootingStrength = 0f;
+    public float maxShootingStrength = 3000f;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-     
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButton(0))
+        {
+            shootingStrength += 20f;
+        }
+        else if (!Input.GetMouseButtonDown(0) && shootingStrength > .1f)
         {
             GameObject arrowObject = Instantiate(arrowPrefab) as GameObject;
             arrowObject.transform.position = transform.position + transform.forward;
             arrowObject.transform.forward = transform.forward;
-            arrowObject.GetComponent<Rigidbody>().AddForce(transform.forward * speed);
-            
+            arrowObject.GetComponent<Rigidbody>().AddForce(transform.forward * shootingStrength);
+            shootingStrength = 0;
+        }
+
+        if (shootingStrength >= maxShootingStrength)
+        {
+            shootingStrength = maxShootingStrength;
         }
     }
 }
