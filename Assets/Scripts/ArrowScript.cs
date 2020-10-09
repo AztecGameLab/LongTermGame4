e88185b0f03f248038ac25b39952c7e569d12350
mattkmaier <70078107+mattkmaier@users.noreply.github.com>
2020-10-09 14:48:57 -0700

    Arrows will freeze in place when coliding with objects

---
+++ Assets/Scripts/ArrowScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    Rigidbody arrowRB;
+
+    private void Start()
+    {
+        arrowRB = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        /*
+         * Not sure what would be the most efficient could be done in one of multible ways
+         * arrowRB.sleep(); but if arrow is hit by another arrow they both fall
+         * 
+         * OR
+         *  
+         * arrowRB.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ |
+         *                      RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
+         *  Like with the implementation bellow arrows stack with each other. 
+         */
+        arrowRB.isKinematic = true;
+        arrowRB.velocity = Vector3.zero;
+    }
+}
