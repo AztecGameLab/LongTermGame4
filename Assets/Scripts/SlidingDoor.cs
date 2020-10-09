20ec1e5e2c883c0435e4671fc690857187989d87
Selfrom <kanghyuklee1@gmail.com>
2020-10-09 00:15:19 -0700

    Improved SlidingDoor
    Fixed bug which allowed for moving beyond intended positions.
    
    Added TimeToOpen as a public float parameter.
    
    Allowed switching of direction mid movement. Speed of doors should remain constant.
    
    Changed the demo sliding door scene to fully implement the double doors.

--- Assets/Scripts/SlidingDoor.cs
+++ Assets/Scripts/SlidingDoor.cs
@@ -5,30 +5,32 @@ using UnityEngine;
 public class SlidingDoor : MonoBehaviour
 {
     public GameObject Door;
-    public Vector3 OpenDirection;
-    private bool IsOpen = false;
+    public Vector3 OpenDirection = Vector3.up;
+    public float TimeToOpen = 1;
+
+    private Vector3 ClosedPosition;
+    private Vector3 OpenPosition;
 
     private void Start()
     {
-        
+        // Start wherever the door
+        ClosedPosition = Door.transform.position;
+        // Move a distance that is proportional to the dimension of the door we are moving along (aka move up the height of the door with a y transform of 1)
+        OpenPosition = Door.transform.position + Vector3.Scale(Door.transform.localScale, OpenDirection);
     }
 
     private void Update()
     {
+        // These triggers are meant to be overriden
+
         if (Input.GetMouseButtonDown(0))
         {
-            if (!IsOpen)
-            {
-                Open();
-            }
+            Open();
         }
 
         if (Input.GetMouseButtonDown(1))
         {
-            if (IsOpen)
-            {
-                Close();
-            }
+            Close();
         }
     }
 
@@ -38,31 +40,41 @@ public class SlidingDoor : MonoBehaviour
     public void Open()
     {
         StopAllCoroutines();
-        StartCoroutine(Slide(OpenDirection, 2f));
+        StartCoroutine(OpenClose(true));
     }
 
     public void Close()
     {
         StopAllCoroutines();
-        StartCoroutine(Slide(-OpenDirection, 2f));
+        StartCoroutine(OpenClose(false));
     }
 
 
-    IEnumerator Slide(Vector3 direction, float time)
+    private IEnumerator OpenClose(bool open)
     {
-        Vector3 startPos = Door.transform.position;
-        Vector3 finalPos = Door.transform.position + Vector3.Scale(Door.transform.localScale, direction);
+        // Start where ever the door currently is
+        Vector3 StartPos = Door.transform.position;
+        Vector3 FinalPos;
 
-        float elapsedTime = 0;
-        while (elapsedTime < time)
+        // Go to where we're supposed to
+        if (open)
+            FinalPos = OpenPosition;
+        else
+            FinalPos = ClosedPosition;
+
+
+        float ElapsedTime = 0;
+        // Take time proportional to the distance we have to go
+        float TotalTime = (FinalPos - StartPos).magnitude/(OpenPosition - ClosedPosition).magnitude * TimeToOpen;
+
+
+        while (ElapsedTime < TotalTime)
         {
-            Door.transform.position = Vector3.Lerp(startPos, finalPos, (elapsedTime / time));
-            elapsedTime += Time.deltaTime;
+            // Move the door by using Lerp
+            Door.transform.position = Vector3.Lerp(StartPos, FinalPos, ElapsedTime / TotalTime);
+            ElapsedTime += Time.deltaTime;
             yield return null;
         }
-
-        IsOpen = !IsOpen;
-
     }
 
 
