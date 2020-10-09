d20ce41e3f8c8be8324163fa22eba0af083dfe99
Selfrom <kanghyuklee1@gmail.com>
2020-10-08 23:27:36 -0700

    Implemented using animations and coroutines
    Added Animations folder and implemented Sliding Door using animations. Started working on Sliding Door using CoRoutines.

---
+++ Assets/Scripts/SlidingDoor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingDoor : MonoBehaviour
+{
+    public List<GameObject> doors;
+    public List<Animator> animators;
+
+    private void Start()
+    {
+        foreach (GameObject door in doors)
+            animators.Add(door.GetComponent<Animator>());
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Open();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Close();
+        }
+    }
+
+
+
+
+    public void Open()
+    {
+        Slide(true);
+    }
+
+    public void Close()
+    {
+        Slide(false);
+    }
+
+    private void Slide(bool state)
+    {
+        foreach (Animator animator in animators)
+        {
+            animator.SetBool("slide", state);
+            Debug.Log("Set slide to " + state);
+        }
+    }
+
+
+}
