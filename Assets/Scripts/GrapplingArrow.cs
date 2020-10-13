d4dbfd69412c73fde699c83a617fba0511ec9732
Brandon Rendon <brendon9463@sdsu.edu>
2020-10-13 16:21:17 -0700

    Created Grappling Arrow Script and Grappling Arrow prefab
    Grappling Arrow Script currently tracks the location of the player, arrow, and collided object and also tracks the mass of the collided object.

---
+++ Assets/Scripts/GrapplingArrow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingArrow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        var player = GameObject.Find("Player");
+        Vector3 playerPosition = player.transform.position;
+        Vector3 arrowPosition = this.transform.position;
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Vector3 collisionPosition = collision.transform.position;
+            if(collision.rigidbody != null)
+            {
+                Debug.Log(collision.rigidbody.mass);
+                if (collision.rigidbody.mass >4)
+                {
+                    Debug.Log("You go to it");
+                }
+                else
+                {
+                    Debug.Log("It go to you");
+                }
+            }
+     
+            
+            Debug.Log(arrowPosition);
+            Debug.Log(playerPosition);
+            Debug.Log(collisionPosition);
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+        if (collision.relativeVelocity.magnitude > 2)
+            Debug.Log("relativeVelocity.magnitude > 2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
