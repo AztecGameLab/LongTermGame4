f200192b47363f56fddf7594b067b4a41f5b9cac
poetahto <walls.dk@gmail.com>
2020-10-14 21:20:44 -0700

    good start on audio manager

---
+++ Assets/Scripts/Audio Scripts/Sound.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Sound", menuName = "Custom/Sound")]
+public class Sound : ScriptableObject
+{
+    [SerializeField] private AudioClip clip = default;
+    [SerializeField] private float volume = 1f;
+
+    public AudioClip Clip => clip;
+    public float Volume => volume;
+
+    public void ApplyToSource(AudioSource source)
+    {
+        source.clip = Clip;
+        source.volume = Volume;
+    }
+}
---
+++ Assets/Scripts/Audio Scripts/TestSounds.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Media;
+using UnityEngine;
+
+public class TestSounds : MonoBehaviour
+{
+    [SerializeField] private Sound arrowHit = default;
+    [SerializeField] private Sound arrowShootIce = default;
+    [SerializeField] private Sound arrowShootFire = default;
+    [SerializeField] private Sound music = default;
+    
+    private AudioManager _audioManager;
+    private bool _playingMusic;
+    
+    private void Awake()
+    {
+        _audioManager = AudioManager.Instance();
+        _playingMusic = false;
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("arrow-hit"))
+        {
+            _audioManager.PlayOneShot(arrowHit);
+        }
+
+        if (GUILayout.Button("arrow-shoot-ice"))
+        {
+            _audioManager.PlayOneShot(arrowShootIce);
+        }
+        
+        if (GUILayout.Button("arrow-shoot-fire"))
+        {
+            _audioManager.PlayOneShot(arrowShootFire);
+        }
+
+        if (_playingMusic == GUILayout.Toggle(_playingMusic, "music")) return;
+        _playingMusic = !_playingMusic;
+
+        if (_playingMusic)
+        {
+            _audioManager.PlayMusic(music);
+        }
+        else
+        {
+            _audioManager.StopMusic(music);
+        }
+    }
+}
