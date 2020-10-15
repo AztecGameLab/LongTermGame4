150ae43d1e0c047639b485fdbd5b6aebc797ab30
poetahto <walls.dk@gmail.com>
2020-10-14 23:23:08 -0700

    random stuff

--- Assets/Scripts/Audio Scripts/Sound.cs
+++ Assets/Scripts/Audio Scripts/Sound.cs
@@ -6,13 +6,16 @@ public class Sound : ScriptableObject
 {
     [SerializeField] private AudioClip clip = default;
     [SerializeField] private float volume = 1f;
-
+    [SerializeField] private bool looping = false;
+    
     public AudioClip Clip => clip;
     public float Volume => volume;
+    public bool Looping => looping;
 
     public void ApplyToSource(AudioSource source)
     {
         source.clip = Clip;
         source.volume = Volume;
+        source.loop = Looping;
     }
 }
