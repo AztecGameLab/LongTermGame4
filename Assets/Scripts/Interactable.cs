350bc0bc43f5b2238ed4cbcd77568c8d502716aa
poetahto <walls.dk@gmail.com>
2020-10-20 20:14:07 -0700

    added holdable objects

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        PlayerInteract.OnInteract += CheckForInteraction;
+    }
+
+    private void OnDisable()
+    {
+        PlayerInteract.OnInteract -= CheckForInteraction;
+    }
+
+    private void CheckForInteraction(Transform target, Transform user)
+    {
+        if (target != transform)
+        {
+            OnMissedInteract();
+        }
+        else
+        {
+            OnInteract(user);
+        }
+    }
+
+    protected abstract void OnInteract(Transform userTransform);
+    protected virtual void OnMissedInteract() { /* Do nothing */ }
+}
