4532e5fef9c6ce57bb7492c0ddf23e701a28a522
Fallium <archer.hovey@gmail.com>
2020-10-22 13:28:25 -0700

    added two bools for useTrigger and UseCollision. Also a list of tags to check instead of just "arrow"
    For some reason OnCollisionExit always triggers right after OnCollisionEnter. Also The player works with triggers, but not collisions

---
+++ Assets/Scripts/EventTrigger.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Linq;
+
+[RequireComponent(typeof(Collider))]
+public class EventTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private string[] tagsToTrigger;
+
+    [SerializeField]
+    private bool useTrigger;
+
+    public UnityEvent triggerEnter;
+    public UnityEvent triggerExit;
+
+    [SerializeField]
+    private bool useCollision;
+    public UnityEvent collisionStart;
+    public UnityEvent collisionEnd;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (useTrigger && tagsToTrigger.Contains(other.tag))
+        {
+            triggerEnter.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (useTrigger && tagsToTrigger.Contains(other.tag))
+        {
+            triggerExit.Invoke();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (useCollision && tagsToTrigger.Contains(collision.gameObject.tag))
+        {
+            collisionStart.Invoke();
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (useCollision && tagsToTrigger.Contains(collision.gameObject.tag))
+        {
+            collisionEnd.Invoke();
+        }
+    }
+}
