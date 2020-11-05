f5a2eba8a7e25fe6d63fb62c7eb7398d3bd410ba
Kain Shepard <kainkun100@gmail.com>
2020-11-04 21:26:37 -0800

    antigrav arrows and  fixed arrow rotation

---
+++ Assets/Scripts/AntiGravArrow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntiGravArrow : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+        if (rb && !rb.isKinematic)
+        {
+            AntiGrav ag = other.gameObject.GetComponent<AntiGrav>();
+            if (ag)
+            {
+                Destroy(ag);
+            }
+            else
+            {
+                other.gameObject.AddComponent<AntiGrav>();
+            }
+        }
+    }
+}
