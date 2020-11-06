92d474229e19f9545675e27d0addf4c1817989c3
maez-vid <meziiro@gmail.com>
2020-11-06 15:26:27 -0800

    Changing code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,60 +5,72 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float moveSpeed = 5f;
-    public float gravity = 15f;
-    public float jumpSpeed = 2f;
+    public float moveSpeed = 10f;
+    public float rotateVerticleSpeed = 4;
+    public float rotateHorizontalSpeed = 4;
+    public float jumpSpeed = 8f;
+    public Rigidbody body;
+    public Camera camera;
 
-    private CharacterController charController;
-    
-
-    Vector3 velocity;
-    
-
+    private bool ground = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        charController = GetComponent<CharacterController>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Looking
+        Vector3 cameraAngle = camera.transform.rotation.eulerAngles;
+        float rotateVertical = Input.GetAxis("Mouse Y") * rotateVerticleSpeed;
+        float angleRotateX = cameraAngle.x - rotateVertical;
+        if (angleRotateX > 180)
+        {
+            angleRotateX = angleRotateX - 360;
+        }
+        angleRotateX = Mathf.Clamp(angleRotateX, -60, 60);
+        rotateVertical = cameraAngle.x - angleRotateX;
+        camera.transform.Rotate(-rotateVertical, 0f, 0f);
+
+        float rotateHorizontal = Input.GetAxis("Mouse X") * rotateHorizontalSpeed;
+        transform.Rotate(0f, rotateHorizontal, 0f);
 
         //Movement
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        charController.Move(move * moveSpeed * Time.deltaTime);
-
-        velocity.y -= gravity * Time.deltaTime;
-
-        charController.Move(velocity * Time.deltaTime);
-
+        Vector3 movement = new Vector3(x * 20, 0f, z * 20);
+        transform.Translate(movement * Time.deltaTime);
 
         //Jump
-        
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (charController.isGrounded)
+            if (ground == true)
             {
-                velocity.y = Mathf.Sqrt(jumpSpeed * 2f * gravity);
-                charController.slopeLimit = 90f;
-
+                body.AddForce(Vector3.up * jumpSpeed * 30);
             }
-            
-        }
-        if (charController.isGrounded && velocity.y < 0)
-        {
-            velocity.y = 0f;
-            charController.slopeLimit = 45f;
 
         }
-        
-       
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            ground = true;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            ground = false;
+        }
     }
 
 }
