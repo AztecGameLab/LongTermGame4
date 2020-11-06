bc4d12313489175b943988891668d6bca17772b5
poetahto <walls.dk@gmail.com>
2020-11-05 19:34:16 -0800

    directory cleanup, added GAUNTLET!

---
+++ Assets/Scripts/Timer Scripts/GauntletController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GauntletController : MonoBehaviour
+{
+    [SerializeField] private List<ArrowTrigger> inactiveTargets = default;
+    [SerializeField] private UnityEvent onSuccess = default;
+    [SerializeField] private UnityEvent onFail = default;
+    
+    private int MaxCharges => inactiveTargets.Count;
+    private List<ArrowTrigger> _activeTargets = new List<ArrowTrigger>();
+    private bool _acceptingCharge = false;
+    private bool Charged => charges >= MaxCharges;
+    private int charges;
+    
+    private void OnEnable()
+    {
+        foreach (var target in inactiveTargets)
+        {
+            target.triggerEnter.AddListener(OnTargetActivate);
+            target.triggerExit.AddListener(OnTargetDeactivate);
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var target in inactiveTargets)
+        {
+            target.triggerEnter.RemoveListener(OnTargetActivate);
+            target.triggerExit.RemoveListener(OnTargetDeactivate);
+        }
+    }
+
+    public void StartGauntlet()
+    {
+        charges = 0;
+        _acceptingCharge = true;
+    }
+
+    public void EndGauntlet()
+    {
+        _acceptingCharge = false;
+        if (!Charged) onFail.Invoke();
+    }
+
+    private void OnTargetActivate()
+    {
+        if (!_acceptingCharge || Charged) return;
+        charges++;
+        if (Charged) onSuccess.Invoke();
+    }
+    
+    private void OnTargetDeactivate()
+    {
+        if (!_acceptingCharge || charges <= 0) return;
+        charges--;
+    }
+}
