12a6ca3cbe3fae2ee823437c871491633a8dad38
MatthewMorisky <matthew.morisky@gmail.com>
2020-11-25 15:30:56 -0800

    Fixes to Tutorial UI
    Implemented Kain's suggestions like having an area for collisions rather than manually entered numbers. Also simplified the text tutorial code.

--- Assets/Scripts/TutorialUI.cs
+++ Assets/Scripts/TutorialUI.cs
@@ -5,35 +5,26 @@ using UnityEngine;
 public class TutorialUI : MonoBehaviour
 {
     public GameObject TutorialBackground;
-    public GameObject Player;
-    public bool TutorialCompleted = false;
-    public int TutorialXMin;
-    public int TutorialXMax;
-    public int TutorialZMin;
-    public int TutorialZMax;
-    
-    void Start()
+
+    public void Start()
     {
-        //Find the player
-        Player = GameObject.FindGameObjectWithTag("Player");
+        TutorialBackground.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowTutorial()
     {
-        //If the tutorial hasn't been completed and the player is within the tutorial bounds
-        if (!TutorialCompleted &&
-            Player.transform.position.x > TutorialXMin && Player.transform.position.x < TutorialXMax &&
-            Player.transform.position.z > TutorialZMin && Player.transform.position.z < TutorialZMax)
-        {
-            //Activate the tutorial
-            TutorialBackground.SetActive(true);
-        }
-        //Otherwise if the player has completed this tutorial or they are outside of the tutorial bounds
-        else
-        {
-            //Deactivate the tutorial
-            TutorialBackground.SetActive(false);
-        }
+        TutorialBackground.SetActive(true);
     }
+
+    public void HideTutorial()
+    {
+        //Deactivate the tutorial
+        TutorialBackground.SetActive(false);
+    }
+    public void CompleteTutorial()
+    {
+        TutorialBackground.SetActive(false);
+        this.enabled = false;
+    }
+    
 }
