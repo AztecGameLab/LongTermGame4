acfe68d427bbcd04b8cfd77e964476fa468c69e8
poetahto <walls.dk@gmail.com>
2020-11-30 12:18:59 -0800

    added menu sounds!

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -3,18 +3,40 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    [SerializeField] private Sound whooshSound = default;
     [SerializeField] private Sound lightClick = default;
     [SerializeField] private Sound heavyClick = default;
+    [SerializeField] private Sound mainMusic = default;
 
+    private SoundInstance _whooshSound;
+    private SoundInstance _lightClick;
+    private SoundInstance _heavyClick;
     private AudioManager _audioManager;
 
+    private static bool _playingMusic = false;
+    private static SoundInstance _mainMusic;
+    
     private void Start()
     {
+        // Initialize sounds
+        _whooshSound = whooshSound.GenerateInstance();
+        _lightClick = lightClick.GenerateInstance();
+        _heavyClick = heavyClick.GenerateInstance();
         _audioManager = AudioManager.Instance();
+
+        if (!_playingMusic)
+        {
+            _mainMusic = mainMusic.GenerateInstance();
+            _audioManager.PlaySound(_mainMusic);
+            _playingMusic = true;
+        }
     }
+
+    // Public Scene Changing Methods
 
     public void LoadSceneCredits()
     {
+        PlayWhoosh();
         SceneManager.LoadScene("MainMenuCredits");
     }
     
@@ -30,22 +52,33 @@ public class ButtonManager : MonoBehaviour
     
     public void LoadScenePlay()
     {
+        PlayWhoosh();
+        Debug.Log("stop");
+        _audioManager.StopSound(_mainMusic);
+        _playingMusic = false;
         SceneManager.LoadScene("Play");
     }
 
     public void LoadSceneMainMenu()
     {
+        PlayWhoosh();
         SceneManager.LoadScene("MainMenu");
     }
 
+    // Public Sound-Playing Methods
+    
     public void PlayLightClick()
     {
-        _audioManager.PlaySound(lightClick);
+        _audioManager.PlaySound(_lightClick);
     }
 
-    public void PlayHeavyClip()
+    public void PlayHeavyClick()
     {
-        _audioManager.PlaySound(heavyClick);
+        _audioManager.PlaySound(_heavyClick);
     }
 
+    public void PlayWhoosh()
+    {
+        _audioManager.PlaySound(_whooshSound);
+    }
 }
