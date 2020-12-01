3cc4a92ec6911a98d85b479de3d9e35f4768db6e
poetahto <walls.dk@gmail.com>
2020-11-30 22:04:35 -0800

    started refactor of grapple

--- Assets/Scripts/GrapplingArrow.cs
+++ Assets/Scripts/GrapplingArrow.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 
 public class GrapplingArrow : MonoBehaviour
@@ -16,72 +15,62 @@ public class GrapplingArrow : MonoBehaviour
     private SoundInstance _fallSound;
     private AudioManager _audioManager;
 
-    public static GrapplingArrow currentArrow;
-    PlayerManager player = PlayerManager.instance;
-    public float moveSpeed = 1;
-    public float massThreshold = 4;
-    public float pullRadiusThreshold = 4;
-    Rigidbody arrowRB;
-    Rigidbody playerRigid;
-    public bool isPulling;
-    public bool stopPull = false;
-    private bool destroyLine = false;
-    private bool isDestroyed = false;
-    public Material ArrowRopeMaterial;
-    LineRenderer line;
+    [Header("Grapple Pull Settings")]
+    [SerializeField] private Material arrowRopeMaterial;
+    [SerializeField] private float moveSpeed = 1;
+    [SerializeField] private float massThreshold = 4;
+    [SerializeField] private float pullRadiusThreshold = 4;
 
-    // Start is called before the first frame update
-    void Start()
+    public static GrapplingArrow CurrentArrow;
+    
+    private bool _collided = false;
+    private bool _stopPull = false;
+    private PlayerManager _player;
+    private Rigidbody _playerRb;
+    private LineRenderer _line;
+
+    private void Awake()
     {
         // initialize sounds
-        _audioManager = AudioManager.Instance();
         _launchSound = launchSound.GenerateInstance();
         _hitSound = hitSound.GenerateInstance();
         _snapSound = snapSound.GenerateInstance();
         _fallSound = fallSound.GenerateInstance();
         
-        _audioManager.PlaySound(_launchSound);
+        // initialize line renderer
+        _line = gameObject.AddComponent<LineRenderer>();
+        _line.material = arrowRopeMaterial == null ? new Material(Shader.Find("Sprites/Default")) : arrowRopeMaterial;
+        _line.textureMode = LineTextureMode.Tile;
+        _line.widthMultiplier = 0.05f;
+        _line.positionCount = 2;
         
-        currentArrow = this;
-        arrowRB = GetComponent<Rigidbody>();
-        playerRigid = player.GetComponent<Rigidbody>();
-        stopPull = false;
-        isPulling = false;
-        LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>(); //Creates the LineRenderer component and sets defaults
-        lineRenderer.material = ArrowRopeMaterial == null ? new Material(Shader.Find("Sprites/Default")) : ArrowRopeMaterial; //ADD YOUR MATERIALS HERE @ARTISTS
-        lineRenderer.textureMode = LineTextureMode.Tile;
-        lineRenderer.widthMultiplier = 0.05f;
-        lineRenderer.positionCount = 2;
-        var points = new Vector3[2];
-        points[0] = player.transform.position;
-        points[1] = this.transform.position;
-        lineRenderer.SetPositions(points);
-
+        CurrentArrow = this;
     }
 
-    void Update()
+    private void Start()
     {
-        //Debug.Log("is Pulling (arrow): " + isPulling);
+        // cache singleton references
+        _audioManager = AudioManager.Instance();
+        _player = PlayerManager.instance;
         
-        if (!isDestroyed) //If arrow has been shot and rendere not destroyed, update line vertices
+        _audioManager.PlaySound(_launchSound);
+        _playerRb = _player.GetComponent<Rigidbody>();
+        _line.SetPositions(new[] { _player.transform.position, transform.position });
+    }
+
+    private void Update()
+    {
+        _line.SetPositions(new[] { _player.transform.position, transform.position });
+        
+        if (Input.GetMouseButton(0))
         {
-            line = GetComponent<LineRenderer>();
-            var points = new Vector3[2];
-            points[0] = player.transform.position;
-            points[1] = this.transform.position;
-            line.SetPositions(points);
-            if (destroyLine || Input.GetMouseButton(0))
-            {
-                _audioManager.PlaySound(_snapSound);
-                isDestroyed = true;
-                Destroy(line);
-            }
+            _audioManager.PlaySound(_snapSound);
+            DestroyThis();
         }
     }
-    bool collided;
-    void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
-        if(collided)//so it can only collide once
+        if(_collided)//so it can only collide once
             return;
         
         //if the collided object DOES have a rigid body, then the grapple will work on it
@@ -89,14 +78,12 @@ public class GrapplingArrow : MonoBehaviour
         if (collision.rigidbody == null || !collision.rigidbody.GetComponent<IsGrappable>()) //If object is not grappable, destroy line renderer on arrow and return
         {
             _audioManager.PlaySound(_fallSound, gameObject);
-            destroyLine = true;
             Invoke(nameof(DestroyThis), 1f);
         }
         else if (!collision.gameObject.CompareTag("arrow"))//To keep players from stacking arrows oddly and from grabbing other arrows
         {
-            collided = true;
-            this.transform.parent = collision.transform; 
-            isPulling = true;
+            _collided = true;
+            transform.parent = collision.transform; 
             Destroy(GetComponent<Rigidbody>());
             StartCoroutine(MoveObject(collision));
         }
@@ -104,7 +91,7 @@ public class GrapplingArrow : MonoBehaviour
         //Debug.Log(collision.rigidbody.mass);
     }
 
-    IEnumerator MoveObject(Collision collision)
+    private IEnumerator MoveObject(Collision collision)
     {
         _audioManager.PlaySound(_hitSound, gameObject);
         LineRenderer line = GetComponent<LineRenderer>();
@@ -112,14 +99,17 @@ public class GrapplingArrow : MonoBehaviour
         //If the object is above a certain mass, the object will pull the player. Else, the player pulls the object
         if (collision.rigidbody.mass < massThreshold)
         {
-
+            ///////////////////////////////////////////
+            // CASE: OBJECT is pulled towards player //
+            ///////////////////////////////////////////
+            
             collision.rigidbody.velocity = Vector3.zero;
             collision.rigidbody.useGravity = false;
-            while (currentArrow == this && !stopPull && Vector3.Distance(collision.transform.position, player.transform.position) > pullRadiusThreshold) //Test if we want to stop pulling, if not, continue with lerp
+            while (CurrentArrow == this && !_stopPull && Vector3.Distance(collision.transform.position, _player.transform.position) > pullRadiusThreshold) //Test if we want to stop pulling, if not, continue with lerp
             {
                 
-                collision.transform.position = Vector3.Lerp(collision.transform.position, player.transform.position, moveSpeed * 2 * Time.deltaTime);
-                points[0] = player.transform.position;
+                collision.transform.position = Vector3.Lerp(collision.transform.position, _player.transform.position, moveSpeed * 2 * Time.deltaTime);
+                points[0] = _player.transform.position;
                 points[1] = transform.position;
                 if(line)
                     line.SetPositions(points); // update line vertices
@@ -127,43 +117,46 @@ public class GrapplingArrow : MonoBehaviour
                 yield return null;
             }
             
-            isPulling = false; //Set bool variables back to default
-            stopPull = false;
+            //Set bool variables back to default
+            _stopPull = false;
             collision.rigidbody.useGravity = true;
             Destroy(line);
-            isDestroyed = true; // destroy line renderer when arrow has no more use
             Invoke(nameof(DisposeAudio), 2f);
             yield break;
         }
+        
+        ///////////////////////////////////////////
+        // CASE: PLAYER is pulled towards object //
+        ///////////////////////////////////////////
+        
         //When player is being pulled, player movement must temporarily be disabled to function properly
-        playerRigid.useGravity = false;
-
-
-        while (currentArrow == this && !stopPull && Vector3.Distance(player.transform.position, this.transform.position) > pullRadiusThreshold)//Test if we want to stop pulling, if not, continue with lerp
+        _playerRb.useGravity = false;
+        
+        while (CurrentArrow == this && !_stopPull && Vector3.Distance(_player.transform.position, this.transform.position) > pullRadiusThreshold)//Test if we want to stop pulling, if not, continue with lerp
         {
-            player.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            player.transform.position = Vector3.Lerp(player.transform.position, this.transform.position, moveSpeed * Time.deltaTime);
-            points[0] = player.transform.position;
-            points[1] = this.transform.position;
+            _player.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            _player.transform.position = Vector3.Lerp(_player.transform.position, this.transform.position, moveSpeed * Time.deltaTime);
+            points[0] = _player.transform.position;
+            points[1] = transform.position;
             if(line)
                 line.SetPositions(points); // update line vertices
             yield return null;
         }
 
-        playerRigid.useGravity = true;
-        isPulling = false; //Set bool variables back to default
-        stopPull = false;
+        _playerRb.useGravity = true;
+        //Set bool variables back to default
+        _stopPull = false;
         Destroy(line);
-        isDestroyed = true; // destroy line renderer when arrow has no more use
         //Debug.Log(isPulling);
 
-        currentArrow = null;
+        CurrentArrow = null;
         Invoke(nameof(DisposeAudio), 2f);
     }
 
     private void DestroyThis()
     {
         DisposeAudio();
+        Destroy(_line);
         Destroy(this);
     }
 
