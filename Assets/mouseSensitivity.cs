5f8b0bfcaa4d57067796a97c9afe8196309a22b4
Kain Shepard <kainkun100@gmail.com>
2020-12-04 11:51:04 -0800

    sounds and stuff

--- Assets/mouseSensitivity.cs
+++ Assets/mouseSensitivity.cs
@@ -7,8 +7,7 @@ public class mouseSensitivity : MonoBehaviour
     
     public void SetSensitivity(float x)
     {
-        PlayerManager.instance.s_looking.mouseSenseH = Mathf.Lerp(10,1000, x);
-        PlayerManager.instance.s_looking.mouseSenseV = Mathf.Lerp(10,1000, x);
+        PlayerManager.instance.s_looking.SetSensitivity(Mathf.Lerp(10,1000, x));
     }
     
 }
---
+++ Assets/playSound.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSound : MonoBehaviour
+{
+    [SerializeField] private Sound sound = default;
+    public void PlaySound()
+    {
+        AudioManager.Instance().PlaySound(sound.GenerateInstance(), gameObject);
+    }
+}
