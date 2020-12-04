b4a30e6121b42d585afee4d1bad83b5bf3e665a0
Kain Shepard <kainkun100@gmail.com>
2020-12-04 11:28:16 -0800

    volume and sens

---
+++ Assets/mouseSensitivity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseSensitivity : MonoBehaviour
+{
+    
+    public void SetSensitivity(float x)
+    {
+        PlayerManager.instance.s_looking.mouseSenseH = Mathf.Lerp(10,1000, x);
+        PlayerManager.instance.s_looking.mouseSenseV = Mathf.Lerp(10,1000, x);
+    }
+    
+}
---
+++ Assets/volumeSlider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class volumeSlider : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public void setVolume(float volume)
+    {
+        AudioManager.SetMasterVolume(mixer, volume);
+    }
+}
