0c59c66792b3cca62e7980188bf765ccf158291c
poetahto <walls.dk@gmail.com>
2020-12-04 20:22:10 -0800

    pause menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,43 +1,90 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    bool paused = false;
-    public GameObject PauseGUI;
-    ReloadScene reloadScene;
-    // Start is called before the first frame update
-    void Start()
+    [Header("Pause Menu Settings")]
+    [SerializeField] private GameObject pauseGUI = default;
+    [SerializeField] private Sound menuWhoosh = default;
+    [SerializeField] private Sound menuClick = default;
+    [SerializeField] private Sound menuHit = default;
+
+    [SerializeField] private AudioMixerSnapshot pausedSnapshot = default;
+    [SerializeField] private AudioMixerSnapshot unpausedSnapshot = default;
+    
+    private SoundInstance _menuWhoosh;
+    private SoundInstance _menuClick;
+    private SoundInstance _menuHit;
+    private AudioManager _audioManager;
+    
+    private const float ClickFreq = 0.15f;
+    private ReloadScene _reloadScene;
+    private float _timeSinceClick = 0f;
+    private bool _paused = false;
+
+    private void Start()
     {
-        reloadScene = GameObject.FindObjectOfType<ReloadScene>();
+        _menuWhoosh = menuWhoosh.GenerateInstance();
+        _menuHit = menuHit.GenerateInstance();
+        _menuClick = menuClick.GenerateInstance();
+        _audioManager = AudioManager.Instance();
+        
+        _reloadScene = FindObjectOfType<ReloadScene>();
         Time.timeScale = 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape)){
-            //If we press escape enable/disable the
-            Pause();
+        if(Input.GetKeyDown(KeyCode.Escape)) Pause();
+        _timeSinceClick += Time.unscaledDeltaTime;
+    }
+
+    public void Pause() 
+    {
+        _audioManager.PlaySound(_menuWhoosh);
+
+        if (_paused)
+        {
+            unpausedSnapshot.TransitionTo(0.01f);
+        }
+        else
+        {
+            pausedSnapshot.TransitionTo(0.01f);
+        }
+        
+        _paused = !_paused;
+        Cursor.lockState = (_paused ? CursorLockMode.None : CursorLockMode.Locked);
+        Time.timeScale = 1 * (_paused ? 0 : 1);
+        pauseGUI.SetActive(_paused);
+    }
+
+    public void Restart() 
+    {
+        _audioManager.PlaySound(_menuWhoosh);
+        Time.timeScale = 1;
+        unpausedSnapshot.TransitionTo(0.01f);
+        _reloadScene.ReloadTheScene();
+    }
+
+    public void Quit()
+    {
+        _audioManager.PlaySound(_menuWhoosh);
+        unpausedSnapshot.TransitionTo(0.01f);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void PlaySliderClick()
+    {
+        if (_timeSinceClick > ClickFreq)
+        {
+            _audioManager.PlaySound(_menuClick);
+            _timeSinceClick = 0f;
         }
     }
-
-    public void Pause(){
-        
-        paused = !paused;
-        Cursor.lockState = (paused ? CursorLockMode.None : CursorLockMode.Locked);
-        Time.timeScale = 1 * (paused ? 0 : 1);
-        PauseGUI.SetActive(paused);
-    }
-
-    public void Restart(){
-        Time.timeScale = 1;
-        reloadScene.ReloadTheScene();
-    }
-
-    public void Quit(){
-        SceneManager.LoadScene("MainMenu");
+    
+    public void PlayHoverSound()
+    {
+        _audioManager.PlaySound(_menuHit);
     }
 }
